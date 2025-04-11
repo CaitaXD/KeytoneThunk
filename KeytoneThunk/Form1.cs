6d22ca811be04f4cc3da1a2b8fc940916433015e
Caetano Dassi <caitaxd@gmail.com>
2025-04-11 20:34:07 -0300

    Initial Commit

---
+++ KeytoneThunk/Form1.cs
@@ -0,0 +1,9 @@
+namespace KeytoneThunk;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ KeytoneThunk/MusicPlayer.cs
@@ -0,0 +1,12 @@
+﻿namespace KeytoneThunk;
+
+public class MusicPlayer
+{
+    public Instrument CurrentInstrument;
+    public int Volume { get; }
+    
+    public void Play(TokenStream tokens)
+    {
+        throw new NotImplementedException();
+    }
+}
